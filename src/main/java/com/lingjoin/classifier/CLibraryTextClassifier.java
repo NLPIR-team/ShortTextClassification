bc6803b4448426714b0925f6d4ee8acd6bc7a551
Hongyan Pan <1107724781@qq.com>
2018-06-04 17:34:09 +0800

    support Win64

--- src/main/java/com/lingjoin/classifier/CLibraryTextClassifier.java
+++ src/main/java/com/lingjoin/classifier/CLibraryTextClassifier.java
@@ -6,7 +6,7 @@ import com.sun.jna.Native;
 public interface CLibraryTextClassifier extends Library{
 
 	CLibraryTextClassifier Instance = (CLibraryTextClassifier) Native.loadLibrary("TextClassification", CLibraryTextClassifier.class);
-	
+
     public int DANClassifierInit(String datafile);
     void NewTrain(boolean increament,int ngram,int dim);
     public void AddString(String classname,String content);
